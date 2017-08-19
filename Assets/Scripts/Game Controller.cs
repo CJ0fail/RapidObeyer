46b28d570f4e52246f0af161a1ac2566ebe810cb
Greifvogel <hunt01110@yahoo.de>
2017-08-19 15:04:52 +0200

    New Diagram, base classes

---
+++ Assets/Scripts/Game Controller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	ModelController model;
+	SceneBuilder builder;
+
+	Action transferEvent(Event e) {
+
+		return null;
+	}
+
+	void transferEvent(Event e) {
+		
+	}
+
+	// Use this for initialization
+	void Start () {
+		model = new ModelController ();
+		builder = new SceneBuilder ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
