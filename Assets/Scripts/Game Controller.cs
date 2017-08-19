5223f4bd4d38178649121d95764a2744049f6c98
rostkeks <felixbaral@me.com>
2017-08-19 19:51:10 +0200

    bone animations and first story

--- Assets/Scripts/Game Controller.cs
+++ Assets/Scripts/Game Controller.cs
@@ -4,27 +4,4 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
-
-	ModelController model;
-	SceneBuilder builder;
-
-	Action transferEvent(Event e) {
-
-		return null;
-	}
-
-	void transferEvent(Event e) {
-		
-	}
-
-	// Use this for initialization
-	void Start () {
-		model = new ModelController ();
-		builder = new SceneBuilder ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
